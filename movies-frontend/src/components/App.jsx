2010f7134fb5a392127144111d2383ff15414cb1
Javier <notri80@gmail.com>
2023-08-28 22:29:47 +0200

    Create Navbar component

---
+++ movies-frontend/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar/NavBar";
+
+function App() {
+  return (
+    <div className="app-main-wraper">
+<Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
--- movies-frontend/src/index.js
+++ movies-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './hero.css';
-import App from './App';
+import './css/hero.css';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
