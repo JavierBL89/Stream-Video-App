cb774486e7e99bfbe19384957a9a08538ede4fdc
Javier <notri80@gmail.com>
2023-08-28 22:35:29 +0200

    Create Button component

---
+++ movies-frontend/src/components/Buttton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Button(props){
+
+    return(
+          <button className="btn btn-outline-success" type="submit">Search</button>
+
+    )
+};
+
+export default Button;
