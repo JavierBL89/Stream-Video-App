b27898e3565928df10012652b64c63b0080bd8b9
Javier <notri80@gmail.com>
2023-08-29 16:14:16 +0200

    Create Footer component,and layout

--- movies-frontend/src/components/App.jsx
+++ movies-frontend/src/components/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import AppNavbar from "./Navbar/AppNavbar";
 import WelcomePage from "./WelcomePage/WelcomePage";
+import Footer from "./Footer/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="app-main-wraper">
      <AppNavbar></AppNavbar>
      <WelcomePage/>
+     <Footer/>
 
     </div>
   );
--- movies-frontend/src/components/Footer/Footer.jsx
+++ movies-frontend/src/components/Footer/Footer.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Container";
+import Col from "react-bootstrap/esm/Container";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 
 function Footer(){
 
     return (
-        <></>
+        <Container fluid className="footer-wraper">
+           <Container fluid className="footer-container">
+               <Container fluid className="row">
+                  <Container  fluid className="col-6" >FAQ</Container>
+                  <Container fluid  className="col-6" >Privacy</Container>
+                </Container>
+                <Container fluid className="row" >
+                <Container fluid className="col-6" >Terms of Use</Container>
+                <Container fluid className="col-6" >Contact Us</Container>
+                 </Container>
+                <Container fluid className="row" >
+                <Container  fluid className="col-6" >Account</Container>
+                <Container  fluid className="col-6" >1 of 2</Container>
+                </Container>
+           </Container>
+
+           <Container>
+            <p className="credits text-center">Built by Javier Bastande</p>
+           </Container>
+        </Container>
     )
 }
 
