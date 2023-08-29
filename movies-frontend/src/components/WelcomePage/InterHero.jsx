a52b97672b8e0d1504a601df7b24dee216dbee64
Javier <notri80@gmail.com>
2023-08-29 21:37:38 +0200

    Add content section interHero-1

--- movies-frontend/src/components/WelcomePage/InterHero.jsx
+++ movies-frontend/src/components/WelcomePage/InterHero.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import Container from 'react-bootstrap/Container';
+import Stack from 'react-bootstrap/Stack';
+import { PiDevicesBold } from 'react-icons/pi';
+import { IoIosPeople } from 'react-icons/io';
+import { HiHome } from 'react-icons/hi';
+
 
 
 function InterHero(props){
 
     return(
         <Container fluid className="interHero-container">
-          <Container fluid className={props.className}>
-            <h6>Share with anyone you want</h6>
+          <Container fluid className={`container-fluid content-container ${props.className}`}>
+            <h3 className={props.classname}>{props.title}</h3>
+            <p>{props.text}</p>
+            {/*** Render if condition ****/}
+            {props.className === "interHero-1" ? 
+              <Stack  className="share-icons" direction="horizontal" >
+                <PiDevicesBold fontSize="3.5rem"/>
+                <HiHome fontSize="3rem"/>
+                <IoIosPeople fontSize="3.5rem"/>
+              </Stack> : null
+            }
           </Container>
 
         </Container>
--- movies-frontend/src/components/WelcomePage/WelcomePage.jsx
+++ movies-frontend/src/components/WelcomePage/WelcomePage.jsx
@@ -10,9 +10,13 @@ function WelcomePage(){
     return (
         <Container fluid className="welcome-main-wraper">
           <Hero classContainer="hero-container hero-1"/>
-          <InterHero className="container-fluid content-container container-1"/>
+          <InterHero 
+            className="interHero-1"
+            text="Up to 5 devices"
+            title=" Share with anyone you want"
+            />
           <Hero classContainer="hero-container hero-2"/>
-          <InterHero className="container-fluid content-container container-2"/>
+          <InterHero className="container-fluid content-container interHero-2"/>
         </Container>
     )
 }
