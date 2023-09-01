7c843302c0bf0819b4634dabf53609f357e310eb
Javier <notri80@gmail.com>
2023-09-01 11:37:18 +0200

    Add styling mobile & tablet screen section Hero-1

--- movies-frontend/src/components/WelcomePage/Hero/Hero1.jsx
+++ movies-frontend/src/components/WelcomePage/Hero/Hero1.jsx
@@ -8,18 +8,14 @@ import Button from "react-bootstrap/esm/Button";
 
 function Hero1(props){
 
-    const spacer = `<br/>`
+
 
     return(
       <div>
           <Container fluid className="hero-wraper">
-            <Container fluid className={`hero-container ${props.classContainer}`}>
+            <Container fluid className={`d-none d-lg-block hero-container ${props.classContainer}`}>
               <H1 className="call-to-action-heading"
-                text="Enjoy"/>
-                <H1 className="call-to-action-heading pt-0"
-                text="unlimited"/>
-                <H1 className="call-to-action-heading pt-0"
-                text="content !"/>
+                text="Enjoy unlimited content!"/>
               <H2 className="call-to-action-text"
                  text="films, series, tv programs and more"/>
                  <Container className="call-to-action">
@@ -27,6 +23,20 @@ function Hero1(props){
                    <Button variant="primary">Sign Up</Button>
                  </Container>
             </Container>
+            <Container fluid className={`d-block d-lg-none d-lg-none hero-container ${props.classContainer}`}>
+              <H1 className="call-to-action-heading1"
+                text="Enjoy"/>
+                <H1 className="call-to-action-heading2"
+                text="unlimited"/>
+                <H1 className="call-to-action-heading3"
+                text="content!"/>
+              <H2 className="call-to-action-text"
+                 text="films, series, tv programs and more"/>
+                 <Container className="call-to-action">
+                   <p>START NOW!</p>
+                   <Button variant="primary">Sign Up</Button>
+                 </Container>
+             </Container>
           </Container>
 </div>
     )
