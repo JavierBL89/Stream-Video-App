39bcf0d724a55572a42c73430efa86118205fed4
Javier <notri80@gmail.com>
2023-09-04 15:22:11 +0200

    Add styling for mobile and table screen for section InterHero-2

--- movies-frontend/src/components/WelcomePage/InterHero/InterHero.jsx
+++ movies-frontend/src/components/WelcomePage/InterHero/InterHero.jsx
@@ -8,17 +8,26 @@ import InterHero2 from "./InterHero2";
 
 function InterHero(props){
 
+
     return(
         <Container fluid className="interHero-container">
           {props.className === "interHero-1" ?
            <InterHero1 
             className={props.className}
-            text="Up to 5 devices"
-            title=" Share with anyone you want" />
+            text1="Up to 5 devices"
+            text2=" Share with anyone you want" />
           : <InterHero2 
             className={props.className}
-            title="Because of you..." 
-            text="and for you..."
+            text1="Weekly"
+            text1ClassName="weeklyWord"
+            text2="NEW CONTENT"
+            text2ClassName="newContent"
+
+
+            text3="Enjoy"
+            text4="PREMIUM RELEASES"
+            text3ClassName="enjoyWord"
+             text4ClassName="premiumReleases"
             />
             }
         </Container>
--- movies-frontend/src/components/WelcomePage/InterHero/InterHero2.jsx
+++ movies-frontend/src/components/WelcomePage/InterHero/InterHero2.jsx
@@ -11,13 +11,22 @@ function InterHero2(props){
     return(
         <Container fluid className={`container-fluid content-container ${props.className}`}>
         <Container >
-            <Row><p className={props.classname}>{props.title}</p></Row>
             <Row>
-                <Col><p>{props.text}</p></Col>
+            <Col sm={12} lg={6}>
+              <p className={props.text1ClassName}>{props.text1}</p>
+              <p className={props.text2ClassName}>{props.text2}</p>
+              </Col>
+              
+              <Col sm={12} lg={6}>
+                <p className={props.text3ClassName}>{props.text3}</p>
+                <p className={props.text4ClassName}>{props.text4}</p>
+                </Col>
+              </Row>
+            <Row>
                 <Col><h5>FilFilm</h5></Col>
             </Row>
             <Row>
-                <Button >SingUp</Button>
+                {/* <Button >SingUp</Button> */}
             </Row>
         </Container>
       </Container>
