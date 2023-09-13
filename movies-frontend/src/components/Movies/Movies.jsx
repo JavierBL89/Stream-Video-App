a4fdfc8f3d7b4a65512cddab52bd7209e0747a13
Javier <notri80@gmail.com>
2023-09-13 22:02:20 +0200

    Create Movies component to hold all the movie lists categorized by gender

---
+++ movies-frontend/src/components/Movies/Movies.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "react-bootstrap/esm/Container";
+
+
+function Movies (){
+
+    return (
+        <Container fluid className="movies-wraper">
+            <Container fluid className="movies-container">
+
+            </Container>
+        </Container>
+    )
+}
+
+export default Movies;
